f6e9914d7da6abd62b904ff3ae33a88c1b1914a0
Christoffer Øhrstrøm <chrisnave92@gmail.com>
2013-04-16 13:05:57 +0200

    Changed Flowlayout to BorderLayout in MainFrame.

--- src/framework/MainFrame.java
+++ src/framework/MainFrame.java
@@ -1,8 +1,9 @@
 package framework;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
 
@@ -11,17 +12,26 @@ public class MainFrame extends JFrame {
 	public final static MainFrame instance = new MainFrame();
 	
 	private MainFrame() {
-	this.setLayout(new FlowLayout());
-	this.setResizable(false);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	this.setJMenuBar(MenuBar.instance);
-	this.setSize(width, height);
-	this.setVisible(true);
-	this.add(StatusBar.instance);
-	this.add(ButtonBar.instance);
-	this.add(ResultBar.instance);
-
+    	this.setLayout(new BorderLayout());
+    	this.setResizable(false);
+    	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    	this.setJMenuBar(MenuBar.instance);
+    	this.setSize(width, height);
+    	
+    	this.setContentPane(new MainPanel());
+    	
+    	this.setVisible(true);
 	}
 	
+	private class MainPanel extends JPanel {
+	    
+	    private MainPanel() {
+	        super(new BorderLayout());
+	        this.add(ResultBar.instance, BorderLayout.PAGE_START);
+	        this.add(ButtonBar.instance, BorderLayout.CENTER);
+	        this.add(StatusBar.instance, BorderLayout.PAGE_END);
+	    }
+	    
+	}
 	
 }
