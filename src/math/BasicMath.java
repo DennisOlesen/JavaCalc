0591803b6d76ebd79b83f134771c68686e2e9c10
Christoffer Øhrstrøm <chrisnave92@gmail.com>
2013-04-16 12:50:34 +0200

    Added the class BasicMath for simple math operations.

---
+++ src/math/BasicMath.java
@@ -0,0 +1,43 @@
+package math;
+
+/**
+ * Contains methods for simple math operations like addition, subtraction etc.
+ * Both integers and doubles are supported.
+ * 
+ * @author christoffer
+ */
+public class BasicMath {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+    
+    public static int substract(int a, int b) {
+        return a-b;
+    }
+    
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+    
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+    
+    public static double add(double a, double b) {
+        return a+b;
+    }
+    
+    public static double substract(double a, double b) {
+        return a-b;
+    }
+    
+    public static double multiply(double a, double b) {
+        return a*b;
+    }
+    
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+    
+}
